1d547edc2d462cb433ab59ff32a0aded33166c68
JC <j.c.ashley4363@gmail.com>
2023-10-22 10:02:31 -0500

    ready to deploy

--- src/components/About/Skills.js
+++ src/components/About/Skills.js
@@ -26,7 +26,7 @@ const Skills = () => {
                     return (
                         <li
                             key={index}
-                            className="font-semibold inline-block capitalize text-base xs:text-lg sm:text-xl  md:text-2xl py-2 xs:py-3 sm:py-4 lg:py-5 px-4 xs:px-6 sm:px-8 lg:px-12 border-2 border-solid border-dark dark:border-light rounded mr-3 mb-3 xs:mr-4 xs:mb-4  md:mr-6 md:mb-6 hover:scale-105 transition-all ease duration-200 cursor-pointer dark:font-normal"
+                            className="font-semibold inline-block capitalize text-base xs:text-lg dark:text-dark dark:text sm:text-xl  md:text-2xl py-2 xs:py-3 sm:py-4 lg:py-5 px-4 xs:px-6 sm:px-8 lg:px-12 border-2 border-solid border-dark dark:border-light rounded mr-3 mb-3 xs:mr-4 xs:mb-4  md:mr-6 md:mb-6 hover:scale-105 transition-all ease duration-200 cursor-pointer dark:font-semibold bg-accent/60 dark:bg-accentDark"
                         >
                             {item}
                         </li>
