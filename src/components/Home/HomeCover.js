b22c6d1be330b5309a1b2faf8da33c336a509f3d
JC <j.c.ashley4363@gmail.com>
2023-10-24 20:46:45 -0500

    fixed homecover photo for mobile

--- src/components/Home/HomeCover.js
+++ src/components/Home/HomeCover.js
@@ -11,23 +11,28 @@ function HomeCover({ blogs }) {
     const blog = sortedBlogs[0]
     return (
         <div className='w-full inline-block'>
-            <article className='flex flex-col items-start justify-end mx-10 relative h-[85vh]'>
+            <article className='flex flex-col items-start justify-end mx-5 sm:mx-10 relative h-[60vh] sm:h-[85vh]'>
                 <div className='absolute top-0 left-0 bottom-0 right-0 h-full 
                 bg-gradient-to-b from-transparent from-0% to-dark/90 rounded-3xl z-0 '
                 />
                 <Link href={blog.url}>
                     {console.log(blog)}
+                    <div className="absolute top-0 left-0 right-0 bottom-0 h-full bg-dark/60 dark:bg-dark/40" />
+
 
                     <Image
                         src={blog.image.filePath.replace("../public", "")}
                         placeholder='blur'
                         blurDataURL={blog.image.blurhashDataUrl}
                         alt={blog.title}
+                        // width={blog.image.width}
+                        // height={blog.image.height}
                         fill
                         className='w-full h-full object-center object-cover rounded-3xl -z-10'
                         sizes='100vw'
                         priority
                     />
+                    {/* </div> */}
                 </Link>
 
                 <div className='w-full lg:w-3/4 p-6 sm:p-8 md:p-12  lg:p-16 flex flex-col items-start justify-center z-0 text-light'>
@@ -50,7 +55,7 @@ function HomeCover({ blogs }) {
                     </p>
                 </div>
             </article>
-        </div>
+        </div >
     )
 }
 
