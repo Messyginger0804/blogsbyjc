e36d3153bfa643433c568e82b76d3f381fccae15
JC <j.c.ashley4363@gmail.com>
2023-11-08 22:44:43 -0600

    worked on view all button and category page. it is ready for when needed

--- src/components/Home/FeatuedPosts.js
+++ src/components/Home/FeatuedPosts.js
@@ -2,6 +2,7 @@ import { sortBlogs } from '@/utils';
 import React from 'react'
 import BlogLayoutOne from '../Blog/BlogLayoutOne';
 import BlogLayoutTwo from '../Blog/BlogLayoutTwo';
+import Link from 'next/link';
 // import BlogLayoutThree from '../Blog/BlogLayoutThree';
 
 function FeatuedPosts({ blogs }) {
@@ -9,7 +10,9 @@ function FeatuedPosts({ blogs }) {
 
     return (
         <section className="w-full mt-16 sm:mt-24  md:mt-32 px-5 sm:px-10 md:px-24  sxl:px-32 flex flex-col items-center justify-center">
-            <h2 className="w-full inline-block font-bold capitalize text-2xl md:text-4xl dark:text-light">Featured Posts</h2>
+            <h2 className="w-full inline-block font-bold capitalize text-2xl md:text-4xl dark:text-light">Featured Posts
+            </h2>
+
 
             <div className="grid grid-cols-2 grid-rows-2 gap-6  mt-10 sm:mt-16">
                 <article className=" col-span-2  sxl:col-span-1 row-span-2 relative">
@@ -21,11 +24,16 @@ function FeatuedPosts({ blogs }) {
                 </article>
                 <article className="col-span-2 sm:col-span-1 row-span-1 relative">
                     <BlogLayoutTwo blog={sortedBlogs[0]} />
-
                 </article>
+
             </div>
 
+            {/* <Link
+                className="flex justify-end font-medium mt-6 end-0 text-accent dark:text-accentDark underline underline-offset-2 text-base md:text-lg"
+                href='/categories/all'
+            >view all</Link> */}
         </section>
+
     )
 }
 
