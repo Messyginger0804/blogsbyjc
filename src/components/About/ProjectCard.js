ac7b5ef7058a614625eb9ef24bf0bb1b3f425955
JC <j.c.ashley4363@gmail.com>
2024-07-27 17:45:55 -0500

    updated styles for button and cards

--- src/components/About/ProjectCard.js
+++ src/components/About/ProjectCard.js
@@ -6,14 +6,13 @@ export function ProjectCard({ proj }) {
     const maxDescLength = 150;
 
     return (
-        <div className='text-center p-10 rounded-xl w-[280px] h-[400px] my-6 max-w-sm relative shadow-accent dark:shadow-accentDark
+        <div className='text-center p-6 rounded-xl w-[280px] h-[400px] my-6 max-w-sm relative shadow-accent dark:shadow-accentDark
          hover:shadow-xl text-black bg-accent/60 dark:bg-accentDark dark:border-light dark:text-black border-2 border-black hover:scale-105 duration-200'>
 
-            {/* Title at the top */}
-            <h3 className='text-lg font-medium dark:text-black absolute top-4 left-0 right-0 mx-4'>{proj.title}</h3>
+            <h3 className='text-lg font-medium dark:text-black absolute top-4 left-0 right-0'>{proj.title}</h3>
 
-            <div className='flex flex-col justify-center h-[400px]'>
-                <div className='flex justify-center mb-0'>
+            <div className='flex flex-col justify-between h-full pt-12'>
+                <div className='flex justify-center h-[100px]'>
                     <Image
                         className='fit object-center rounded'
                         src={proj.image}
@@ -29,14 +28,14 @@ export function ProjectCard({ proj }) {
                     <h3 className='text-lg font-medium pt-1 pb-1'>{proj.descTitle}</h3>
                     <p className='text-xs'>{truncateText(proj.desc, maxDescLength)}</p>
                 </div>
-            </div>
 
-            {/* Buttons container */}
-            <div className='flex justify-evenly absolute bottom-0 left-0 w-full p-6 z-20'>
-                <a href={proj.rep} className='hover:bg-white p-2 text-sky-400 border-2 border-sky-400 rounded-lg' target='_blank'>
-                    github
-                </a>
-                {button(proj.isPublished, proj.live)}
+                {/* Buttons container */}
+                <div className='flex justify-evenly mt-auto'>
+                    <a href={proj.rep} className='hover:bg-sky-400 bg-slate-50 hover:text-white p-2 text-black dark:hover:bg-accentDark dark:bg-black dark:text-accentDark dark:hover:text-black border-2 border-sky-400 hover:dark:border-black cursor-pointer dark:border-white rounded-lg transition duration-200' target='_blank'>
+                        GitHub
+                    </a>
+                    {button(proj.isPublished, proj.live)}
+                </div>
             </div>
         </div>
     );
