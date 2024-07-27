2d7dd4ad138ccfef650dff0fe493b7475e731228
JC <j.c.ashley4363@gmail.com>
2024-07-26 22:32:26 -0500

    updated about me section need to work on resizing it

--- src/app/(about)/about/page.js
+++ src/app/(about)/about/page.js
@@ -1,5 +1,6 @@
 
 import AboutCover from "@/components/About/AboutCover";
+import Portfolio from "@/components/About/Portfolio";
 import Skills from "@/components/About/Skills";
 import Link from "next/link";
 
@@ -14,6 +15,7 @@ export default function About() {
         <>
             <AboutCover />
             <Skills />
+            <Portfolio />
             <h2 className="mt-8 font-semibold text-lg md:text-2xl self-start mx-5 xs:mx-10 sm:mx-12 md:mx-16 lg:mx-20 text-dark dark:text-light dark:font-normal">
                 Have a project in mind? Reach out to me  <a href="tel:2149493126">📞</a> from <Link href="/contact" className="!underline underline-offset-2"   >here</Link> and let's make it happen.
             </h2>
--- src/components/About/AboutCover.js
+++ src/components/About/AboutCover.js
@@ -1,40 +1,30 @@
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
-import profileCharacter from "../../../public/bitmoji.png"
-import Portfolio from './Portfolio'
+import Image from 'next/image';
+import React from 'react';
+import profileCharacter from "../../../public/bitmoji.png";
 
 const AboutCover = () => {
     return (
-        <>
-            <section className='w-full md:h-[75vh] border-b-2 border-solid border-dark dark:border-light flex flex-col md:flex-row items-center justify-center text-dark dark:text-light'>
-                <div className='w-full md:w-1/2 h-full border-r-2 border-solid border-dark dark:border-light flex justify-center'>
-                    <Image
-                        className='w-4/5  xs:w-3/4 md:w-full h-full object-contain object-center'
-                        src={profileCharacter}
-                        alt="Jeremy Ashley"
-                        priority
-                        sizes="(max-width: 768px) 100vw,(max-width: 1180px) 50vw, 50vw"
-                    />
-                </div>
+        <section className='w-full h-full lg:h-[75vh] border-b-2 border-solid border-dark dark:border-light flex flex-col lg:flex-row items-center justify-center text-dark dark:text-light'>
+            <div className='w-full lg:w-1/2 h-full border-b-2 lg:border-b-0 lg:border-r-2 border-solid border-dark dark:border-light flex justify-center'>
+                <Image
+                    className='w-4/5 xs:w-3/4 lg:w-full h-full object-contain object-center'
+                    src={profileCharacter}
+                    alt="Jeremy Ashley"
+                    priority
+                    sizes="(max-width: 768px) 100vw,(max-width: 1180px) 50vw, 50vw"
+                />
+            </div>
 
-                <div className='w-full md:w-1/2 flex flex-col text-left items-start justify-center px-5 xs:p-10 pb-10 lg:px-16'>
-                    <h2 className='font-bold capitalize text-4xl xs:text-5xl sxl:text-6xl  text-center lg:text-left'>
-                        This is me JC 😏
-                    </h2>
-                    <p className='font-medium mt-4 text-base'>
-                        Welcome to my corner of the digital world! I'm a software engineer with a unique journey that led me to this exciting and ever-evolving field. Before diving into the tech world 🌎, I spent several years as a store manager for Aldi. This experience instilled in me a strong work ethic, exceptional organizational skills, and the ability to thrive in a fast-paced, results-oriented environment.
-                    </p>
-                    <p className='font-medium mt-4 text-base'>
-                        My transition to the world of software engineering wasn't just a career change; it was a pursuit of my true passion. I'm a firm believer that the key to success is continuous growth and challenge. As a software engineer, I've embraced this philosophy wholeheartedly. Each day, I strive to hone my craft, push the boundaries of what I know, and explore the endless possibilities within the world of technology. In this fast-paced, ever-changing field, I've found my true calling. I invite you to join me on this exciting adventure through the world of software development, where every line of code is a step toward innovation and progress.</p>
-                    <p className='font-medium mt-4 text-base'>
-                        Feel free to explore my work, thoughts, and projects, and don't hesitate to get <Link href='/contact' className='hover:underline font-bold'>in touch</Link> if you'd like to collaborate or share your own experiences. Together, we can continue to learn, grow, and shape the future of technology.
-                    </p>
-                </div>
-            </section>
-            <Portfolio />
-        </>
+            <div className='w-full lg:w-1/2 flex flex-col text-left items-start justify-center px-5 xs:p-10 pb-10 lg:px-16'>
+                <h2 className='font-bold capitalize text-4xl xs:text-5xl xl:text-6xl text-center lg:text-left'>
+                    Hey I am JC 😏
+                </h2>
+                <p className='font-medium mt-4 text-xs md:text-lg tracking-widest leading-5 md:leading-8'>
+                    I'm a dedicated and committed Software Engineer, passionate about delivering high-quality solutions. As a father of three, I believe in bringing fun and positivity into challenging situations. With a strong track record of success as a freelance developer and Lead Intern Engineer at iServiceWeb, I've developed a thirst for continuous learning and self-improvement. I aggressively seek out opportunities to enhance my skills and expertise, always looking for ways to improve myself and those around me. My passion lies in the process of creation, and I find joy in the journey of building innovative solutions. With a proven ability to lead teams and deliver results, I'm excited to continue growing as a professional and making meaningful contributions to the tech community.
+                </p>
+            </div>
+        </section>
     )
 }
 
-export default AboutCover
+export default AboutCover;
