dd6eb7b9e38fcfc34c39b31f8a0df198601b4c14
JC <j.c.ashley4363@gmail.com>
2024-07-28 22:25:04 -0500

    updated styles for about me section

--- src/components/About/AboutCover.js
+++ src/components/About/AboutCover.js
@@ -4,8 +4,8 @@ import profileCharacter from "@/components/About/assets/justme.png";
 
 const AboutCover = () => {
     return (
-        <section className="w-full h-auto lg:h-[75vh] min-h-[50vh] border-b-2 border-solid border-dark dark:border-light flex flex-col lg:flex-row items-center justify-center text-dark dark:text-light mt-20">
-            <div className="flex justify-center items-center lg:justify-start mx-auto bg-gradient-to-b from-accent dark:from-accentDark rounded-full w-80 h-80 relative overflow-hidden md:h-96 md:w-96">
+        <section className="w-full h-auto min-h-[50vh] border-b-2 border-solid border-dark dark:border-light flex flex-col items-center justify-center text-dark dark:text-light mt-20">
+            <div className="flex justify-center items-center mx-auto bg-gradient-to-b from-accent dark:from-accentDark rounded-full relative overflow-hidden w-80 h-80 md:w-80 md:h-80 lg:w-96 lg:h-96">
                 <Image
                     src={profileCharacter}
                     className='rounded-lg'
@@ -15,9 +15,9 @@ const AboutCover = () => {
                     style={{ objectFit: "cover" }}
                 />
             </div>
-            <div className='w-full lg:w-1/2 flex flex-col text-left items-start justify-center px-5 xs:px-10 pb-10 lg:px-16 lg:h-[75vh] min-h-[50vh]'>
-                <div className='mt-5 lg:mt-0 h-full overflow-y-auto'>
-                    <h2 className='font-bold capitalize text-4xl xs:text-5xl xl:text-6xl text-center md:text-left'>
+            <div className='w-full flex flex-col text-left items-start justify-center px-5 xs:px-10 pb-10 lg:px-16 min-h-[50vh]'>
+                <div className='h-full overflow-y-auto'>
+                    <h2 className='font-bold capitalize text-4xl xs:text-3xl xl:text-6xl text-center md:text-left'>
                         Hey I am JC 😏
                     </h2>
                     <p className='font-medium mt-4 text-xs md:text-lg tracking-widest leading-5 md:leading-8'>
