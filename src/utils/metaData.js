f03e03078c6352c283a337174aea84622bc05428
JC <j.c.ashley4363@gmail.com>
2024-07-31 10:23:26 -0500

    upated seo and image preview

--- src/utils/metaData.js
+++ src/utils/metaData.js
@@ -1,20 +1,23 @@
 // please update required information
+import headshot from '../components/About/assets/headshot.jpg'
+
 const siteMetadata = {
     title: 'Blogs by JC',
     author: 'JC Ashley',
     headerTitle: 'Next.js Blog',
     description: 'A blog created with Next.js, Tailwind.css and contentlayer. (created with already existing content)',
     language: 'en-us',
-    theme: 'system', // system, dark or light
-    siteUrl: 'https://blogsbyjc.vercel.app/', // your website URL
+    theme: 'system',
+    siteUrl: 'https://blogsbyjc.vercel.app/',
     siteLogo: '/profile.png',
-    socialBanner: '/social-banner.png', // add social banner in the public folder
+    socialBanner: '/social-banner.png',
     email: 'j.c.ashley4363@gmail.com',
     github: 'https://github.com/Messyginger0804',
     twitter: 'https://twitter.com/Messyginger0804',
     linkedin: 'https://www.linkedin.com/in/jeremy-ashley-webdev/',
     portfolio: 'https://jcashleyportfolio.netlify.app/',
     resume: 'https://drive.google.com/file/d/1gF3bfL9F0kn5cWfEy46_2wKaI2k3p-gE/view',
+    portfolioImage: headshot,
     locale: 'en-US',
 }
 
