2d317c41b02c6cb8ac50d64cef6dbc8fc2df871e
JC <j.c.ashley4363@gmail.com>
2024-07-31 09:09:45 -0500

    created component for image and icons

--- src/app/(portfolio)/contact/page.js
+++ src/app/(portfolio)/contact/page.js
@@ -2,6 +2,7 @@ import ContactForm from "@/components/Contact/ContactForm";
 import siteMetadata from "@/utils/metaData";
 import profileCharacter from "@/components/About/assets/justme.png";
 import Image from "next/image";
+import Avator from "@/components/About/Avator";
 
 export const metadata = {
     title: "Contact Me",
@@ -11,15 +12,8 @@ export const metadata = {
 export default function Contact() {
     return (
         <section className="w-full h-auto lg:h-[75vh] border-b-2 border-solid border-dark dark:border-light flex flex-col lg:flex-row items-center justify-center text-dark dark:text-light mt-20">
-            <div className="mx-auto bg-gradient-to-b from-accent dark:from-accentDark rounded-full w-80 h-80 relative overflow-hidden md:h-96 md:w-96">
-                <Image
-                    src={profileCharacter}
-                    className='rounded-lg'
-                    alt="Profile Character"
-                    priority
-                    fill
-                    style={{ objectFit: "cover" }}
-                />
+            <div>
+                <Avator />
             </div>
             <div className="w-full lg:w-1/2 flex flex-col text-left items-start justify-center px-5 xs:px-10 pb-10 lg:px-16 lg:h-[75vh] overflow-y-auto">
                 <h2 className="font-bold capitalize text-2xl xs:text-3xl sm:text-4xl">Let's Connect!</h2>
--- src/components/About/AboutCover.js
+++ src/components/About/AboutCover.js
@@ -1,29 +1,11 @@
 import Image from 'next/image';
 import React from 'react';
-import profileCharacter from "@/components/About/assets/justme.png";
-import { AiFillGithub, AiFillLinkedin, AiFillTwitterCircle } from 'react-icons/ai';
-import { CgFileDocument } from "react-icons/cg";
-import siteMetadata from '@/utils/metaData';
+import Avator from './Avator';
 
 const AboutCover = () => {
     return (
         <section className="w-full h-auto min-h-[50vh] border-b-2 border-solid border-dark dark:border-light flex flex-col items-center justify-center text-dark dark:text-light mt-20">
-            <div className="flex justify-center shadow-lg shadow-accent dark:shadow-light items-center mx-auto bg-gradient-to-b from-accent dark:from-accentDark rounded-full relative overflow-hidden w-80 h-80 md:w-80 md:h-80 lg:w-96 lg:h-96">
-                <Image
-                    src={profileCharacter}
-                    className='rounded-lg'
-                    alt="Profile Character"
-                    priority
-                    fill
-                    style={{ objectFit: "cover" }}
-                />
-            </div>
-            <div className='text-5xl flex justify-center gap-16 py-3 text-accent dark:text-accentDark '>
-                <a href={siteMetadata.linkedin} target='_blank'>
-                    <AiFillLinkedin className='shadow-lg dark:shadow-yellow-50 shadow-accent rounded-full dark:hover:text-accent/60 hover:cursor-pointer' /> </a>
-                <a href={siteMetadata.github} target='_blank'><AiFillGithub className='shadow-lg shadow-accent dark:shadow-yellow-50 rounded-full dark:hover:text-accent/60 hover:cursor-pointer' /></a>
-                <a href={siteMetadata.resume} target='_blank'><CgFileDocument className='shadow-lg shadow-accent dark:shadow-yellow-50 rounded-full dark:hover:text-accent/60 hover:cursor-pointer' /></a>
-            </div>
+            <Avator />
             <div className='w-full flex flex-col text-left items-start justify-center px-5 xs:px-10 pb-10 lg:px-16 min-h-[50vh]'>
                 <div className='h-full overflow-y-auto'>
                     <h2 className='font-bold capitalize mx-16 text-4xl xs:text-3xl xl:text-6xl text-center md:text-left'>
