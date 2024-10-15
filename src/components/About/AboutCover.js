eda7cc544b2ea222e969189cdb276397c6a00e7a
Messyginger0804 <j.c.ashley4363@gmail.com>
2024-10-15 11:50:00 -0500

    updated title a little styles for about me page

--- src/components/About/AboutCover.js
+++ src/components/About/AboutCover.js
@@ -11,7 +11,7 @@ const AboutCover = () => {
                     <h2 className='font-bold capitalize mx-16 text-4xl xs:text-3xl xl:text-6xl text-center md:text-left'>
                         Hey I am JC 😏
                     </h2>
-                    <p className='font-medium mt-4 md:mx-12 text-xs md:text-lg tracking-widest leading-5 md:leading-8'>
+                    <p className='font-medium mt-4 md:mx-12 text-xs md:text-lg tracking-tight leading-2 md:leading-8'>
                         I'm a dedicated and committed Software Engineer, passionate about delivering high-quality solutions. As a father of three, I believe in bringing fun and positivity into challenging situations. With a strong track record of success as a freelance developer and Lead Intern Engineer at iServiceWeb, I've developed a thirst for continuous learning and self-improvement. I aggressively seek out opportunities to enhance my skills and expertise, always looking for ways to improve myself and those around me. My passion lies in the process of creation, and I find joy in the journey of building innovative solutions. With a proven ability to lead teams and deliver results, I'm excited to continue growing as a professional and making meaningful contributions to the tech community.
                     </p>
                 </div>
